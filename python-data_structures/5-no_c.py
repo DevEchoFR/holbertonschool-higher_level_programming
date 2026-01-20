d7e54c5ef05587b97568b829711eae2d49b91349
Vesna David ROSET <snaboy18@gmail.com>
2026-01-20 09:08:20 +0000

    fix: standardize string initialization and variable naming in no_c function

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''
+    new_string = ""
 
-    for c in my_string:
-        if c != 'c' and c != 'C':
-            new_string += c
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
 
     print(new_string)
