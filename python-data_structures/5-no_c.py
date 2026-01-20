1cc4da01b280e907f40f28f70f738ef0b9cf3a1c
Vesna David ROSET <snaboy18@gmail.com>
2026-01-20 09:04:44 +0000

    fix: correct condition in no_c function to properly exclude 'c' and 'C'

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     new_string = ''
 
     for c in my_string:
-        if c is not ('c', 'C'):
+        if c != 'c' and c != 'C':
             new_string += c
 
     print(new_string)
