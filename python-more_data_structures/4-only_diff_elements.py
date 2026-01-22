6a80dca07b96e819f9ff4de3d3f64e6fb4a89db9
Vesna David ROSET <snaboy18@gmail.com>
2026-01-22 21:30:26 +0000

    style: fix indentation in only_diff_elements function

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,11 +2,11 @@
 def only_diff_elements(set_1, set_2):
     result = set()
     for element in set_1:
-         if element not in set_2:
-              result.add(element)
+        if element not in set_2:
+            result.add(element)
 
     for element in set_2:
-         if element not in set_1:
-              result.add(element)
+        if element not in set_1:
+            result.add(element)
 
     return result
