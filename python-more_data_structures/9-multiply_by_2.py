f2c13f6bc1e2f502c9635618c31e1addea0ed243
Vesna David ROSET <snaboy18@gmail.com>
2026-01-23 10:30:03 +0000

    Add function to multiply dictionary values by 2

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/python3
+def multiply_by_2(a_dictionary):
+    new_dic = {}
+
+    for key in a_dictionary:
+        value = a_dictionary[key]
+        new_dic[key] = value * 2
+    return new_dic
