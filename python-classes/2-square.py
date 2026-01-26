721532eab25e06d1726ca7fbf4925e8b67c675fd
Vesna David ROSET <snaboy18@gmail.com>
2026-01-26 13:08:19 +0000

    fix chnage style type over isinstance

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@ class Square:
     """Represents a square."""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
 
         if size < 0:
