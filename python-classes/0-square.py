7f1947a6fd63a9e05787b8d16561d4a6e1f25935
Vesna David ROSET <snaboy18@gmail.com>
2026-01-29 08:00:27 +0000

    fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a Square class 
 used to represent a basic square shape."""
+
+
 class Square:
     """This class represents a square without 
     any defined attributes or behavior."""
