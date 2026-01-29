1d1cf9f163a17dfd17c71f5421ba718e2df66d32
Vesna David ROSET <snaboy18@gmail.com>
2026-01-29 08:13:42 +0000

    fix docstrings comment

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Defines a Square class a private instance attribute."""
+
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size):
+        """Initializes a new Square instance with a given size value."""
         self.__size = size
