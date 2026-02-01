aa6f461302e434704ea38f9b218771968099f13c
Vesna David ROSET <snaboy18@gmail.com>
2026-02-01 22:17:07 +0000

    Add print_square function with proper validation and documentation

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to print a square using the # character.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square made of # characters.
+
+    Args:
+        size (int): length of the square sides
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+    """
+
+    if type(size) is bool:
+        raise TypeError("size must be an integer")
+
+    if type(size) not in int:
+        raise TypeError("size must be an integer")
+
+    if size <= 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
