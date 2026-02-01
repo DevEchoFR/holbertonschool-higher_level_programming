af7e3dd9fe1fd634aa9b5eaa8f2fd9af79983fb9
Vesna David ROSET <snaboy18@gmail.com>
2026-02-01 21:44:15 +0000

    Add matrix_divided function with full validation and rounding

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,45 +1,28 @@
 #!/usr/bin/python3
 """
-Module: 2-matrix_divided
-Contains matrix_divided(matrix, div).
+This module provides a function that divide all elements of a matrix
 """
 
 
 def matrix_divided(matrix, div):
-    """Divide all elements of a matrix by div (rounded to 2 decimals)."""
+    """
+    Divide all elements of a matrix
 
-    if type(div) not in (int, float) or div != div or div in (
-        float('inf'), float('-inf')
-    ):
+    :param matrix: the matrix will be cloned then divided
+    :param div: the divider
+    """
+    if not isinstance(div, (int, float)) or isinstance(div, float):
         raise TypeError("div must be a number")
-
-    if div == 0:
+    elif div == 0:
         raise ZeroDivisionError("division by zero")
-
-    if type(matrix) is not list or matrix == []:
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
-
-    if any(type(row) is not list or row == [] for row in matrix):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
-
-    for row in matrix:
-        for element in row:
-            if type(element) not in (int, float):
-                raise TypeError(
-                    "matrix must be matrix (list of lists) of integers/floats")
-
-    row_length = len(matrix[0])
-    for row in matrix:
-        if len(row) != row_length:
+    try:
+        if matrix and len({len(row) for row in matrix}) > 1:
             raise TypeError("Each row of the matrix must have the same size")
-
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for element in row:
-            new_row.append(round(element / div, 2))
-        new_matrix.append(new_row)
-
-    return new_matrix
+        return [[round(subelement / div, 2) for subelement in element]
+                for element in matrix]
+    except TypeError as error:
+        if "row" in str(error):
+            raise TypeError("Each row of the matrix must have the same size")
+        else:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
