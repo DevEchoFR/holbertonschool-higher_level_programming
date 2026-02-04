428b685b023b2192b020bcb7ef3de2b372e890e9
Vesna David ROSET <snaboy18@gmail.com>
2026-02-04 09:00:27 +0000

    Add BaseGeometry empty class with documentation

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a base class for geometric objects.
+
+The BaseGeometry class is intended to be a parent class
+for other geometry-related classes in this project.
+"""
+
+
+class BaseGeometry:
+    """
+    Represents a base geometry class.
+
+    This class serves as a foundation for geometry classes
+    and does not define any attributes or methods.
+    """
+    pass
