6a3cbc7e4eefa9f71b9eb1a71012872c6a39316e
Vesna David ROSET <snaboy18@gmail.com>
2026-02-10 08:57:07 +0000

    Add write_file function to write text to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 def write_file(filename="", text=""):
     # Open the file using the correct mode
     with open(filename, mode="w") as numbers:
         # Write the text to the file
-        text = numbers.write(________)
+        text = numbers.write(numbers)
 
     # Return the number of characters written
-    return ________
+    return text
