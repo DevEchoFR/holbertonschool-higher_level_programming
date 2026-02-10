4c47740c8d4781055746b5026260ae4882af8a36
Vesna David ROSET <snaboy18@gmail.com>
2026-02-10 08:13:51 +0000

    Add function to read and print file contents

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+def read_file(filename=""):
+    # Open the file using the correct keyword
+    with open(filename, mode="r") as text:
+        # Read the entire file content
+        content = text.read()
+
+        # Print the content to standard output
+        print(content)
